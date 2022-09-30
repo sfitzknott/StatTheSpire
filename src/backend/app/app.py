29582915ef78ddf50605a626ccb4af96766b24d2
sfitzknott <sfitzknott@gmail.com>
2022-09-30 20:58:02 +0100

    db populating from local  added

--- src/backend/app/app.py
+++ src/backend/app/app.py
@@ -1,9 +1,17 @@
+from typing import Collection
 from flask import Flask, jsonify, json
 import os
 import pymongo
 from pymongo import MongoClient, InsertOne
 
 app = Flask(__name__)
+
+client = pymongo.MongoClient('127.0.0.1:27017')
+db = client.statTheSpire
+characters = {'DEFECT' : db.defect,
+              'SILENT': db.silent,
+              'IRONCLAD:': db.ironclad,
+              'WATCHER': db.watcher}
 
 @app.route("/arun")
 def a_run():
@@ -15,8 +23,7 @@ def a_run():
 
 @app.route("/ins")
 def ins(): 
-    client = pymongo.MongoClient('127.0.0.1:27017')
-    db = client.statTheSpire
+
     collection = db.defect
     requesting = []
 
@@ -28,3 +35,23 @@ def ins():
     result = collection.bulk_write(requesting)
     client.close()
     return {}
+
+@app.route("/createdb")
+def createdb(): 
+    client = pymongo.MongoClient('127.0.0.1:27017')
+    db = client.statTheSpire
+    [populate_collection(v, k) for k,v in characters]
+    client.close()
+    return "creation succesful"
+
+#Populates mongo with runs currently in runs folder
+def populate_collection(collection: Collection, name: str):
+    requesting: list[InsertOne] = []
+    dir: str = os.path.join(app.static_folder, 'runs', name)
+    with os.scandir(dir) as runs:
+        for file in runs:
+            if file.name.endswith(".run") and file.is_file():
+                with open(file) as run:
+                    requesting.append(InsertOne(json.load(run)))
+
+    result = collection.bulk_write(requesting)
